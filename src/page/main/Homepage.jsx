6ab30e160e72dcb73c0b01afca8e5c01218f1287
ƠncủaKhải <tranmanhkhai2004@gmail.com>
2024-07-24 12:40:20 +0700

    Fix bugs

--- src/page/main/Homepage.jsx
+++ src/page/main/Homepage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import "./styles.css"
 const Homepage = () => {
     return (
-        <div>Homepage</div>
+        <div className="home-page">
+            <h2>Technology Price History & Tracker</h2>
+            <p>Price Tracker, Price History Charts and Price Drop Alerts for Technology products.</p>
+        </div >
     )
 }
 
