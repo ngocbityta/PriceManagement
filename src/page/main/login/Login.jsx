15f96c3606f6094d060454e6ea49e75c5f30b888
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2024-07-25 18:29:53 +0700

    login v2

--- src/page/main/login/Login.jsx
+++ src/page/main/login/Login.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import "./style.css"
+import React from 'react';
+import "./style.css";
 
 function Login() {
     return (
-        <div id="Login">
-            <div id="login-header">Log In</div>
-            <div id="login-with-email">
-                Login with Email
-                <div id="email-input-wrapper">
-                    <div id="email-text" class="login-with-email-text">Email</div>
-                    <input type="email" name="Email" id="email-input" />
-                </div>
-                <div id="password-input-wrapper">
-                    <div id="password-text" class="login-with-email-text">Password</div>
-                    <input type="password" name="Password" id="password-input" />
-                </div>
+        <div id="wrapper">
+            <div class="login-form">
+                <h2>Login with Email</h2>
+                <form action="#" method="post">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required></input>
 
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required></input>
+                    <div id='space'></div>
+
+
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
+            <div id="forgot-password">
+                <a href="#" >Forgot Password?</a>
+            </div>
+            <div id="create-new-account">
+                <h3>New to PriceTrack?</h3>
+                <button type="button" href="#">Create New Account</button>
             </div>
         </div>
+
     )
 }
 
