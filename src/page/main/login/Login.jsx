0ba3ad44a8585146d7d05bdbd4afd4fbcf1bf21c
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2024-07-26 21:52:20 +0700

    update css log in

--- src/page/main/login/Login.jsx
+++ src/page/main/login/Login.jsx
@@ -4,6 +4,13 @@ import "./style.css";
 function Login() {
     return (
         <div id="wrapper">
+            <div id="title">Log In</div>
+            <div id="login-with-third-party">
+                <a href="#" title='Login with Google'>
+                    <span class="google-image"></span>
+                    <span class="login-text">Login with Google</span>
+                </a>
+            </div>
             <div class="login-form">
                 <h2>Login with Email</h2>
                 <form action="#" method="post">
@@ -17,10 +24,11 @@ function Login() {
 
                     <button type="submit">Log In</button>
                 </form>
+                <div id="forgot-password">
+                    <a href="#" >Forgot Password?</a>
+                </div>
             </div>
-            <div id="forgot-password">
-                <a href="#" >Forgot Password?</a>
-            </div>
+
             <div id="create-new-account">
                 <h3>New to PriceTrack?</h3>
                 <button type="button" href="#">Create New Account</button>
